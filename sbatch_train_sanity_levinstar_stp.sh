93779a32016a491a9c7dafe14c0092d9e553d303
Ken Tjhia <KenTjhia@gmail.com>
2024-06-27 01:08:14 -0600

    add sanity check CC scripts

--- sbatch_train_sanity_levinstar_stp.sh
+++ sbatch_train_sanity_levinstar_stp.sh
@@ -4,13 +4,13 @@
 declare -a losses=("LevinLoss")
 output="output_train_sanity_stp/"
 domain_name="5x5-stp-"
+algorithm="LevinStar"
 
+#heuristic_scheme=("--learned-heuristic --default-heuristic" "--default-heuristic" "--learned-heuristic")
 heuristic_scheme=("--learned-heuristic")
-algorithm="Levin"
 
 scheduler="online"
 
-#for iter in {1..5}; do
 for iter in {1..1}; do
 	for scheme in "${heuristic_scheme[@]}"; do
 		for loss in ${losses[@]}; do
@@ -20,13 +20,9 @@ for iter in {1..1}; do
 			name_scheme=${name_scheme//-heuristic/}
 			name_scheme=${name_scheme//--/-}
 			output_exp="${output}${lower_algorithm}-${lower_loss}${name_scheme}-${scheduler}-v${iter}"
-            model=${domain_name}${lower_algorithm}-${lower_loss}${name_scheme}-${scheduler}-v${iter}
-
-			#echo ${output_exp}
-			#echo ${model}
+			model=${domain_name}${lower_algorithm}-${lower_loss}${name_scheme}-${scheduler}-v${iter}
 
 			sbatch --output=${output_exp} --export=scheme="${scheme}",algorithm=${algorithm},loss=${loss},model=${model} run_bootstrap_train_sanity_stp.sh
 		done
 	done
 done
-
